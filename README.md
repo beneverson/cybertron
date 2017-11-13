642d4882ef98f949a66c5923f019edb0c5c13348
Ben Everson <ben@svds.com>
2017-11-13 14:04:19 -0800

    added dummyencoder

---
+++ README.md
@@ -0,0 +1,2 @@
+# cybertron
+A library of scikit-learn compatible Transformers for common data science tasks.
--- cybertron/transformers.py
+++ cybertron/transformers.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.proprocessing import LabelEncoder
 
@@ -33,6 +35,9 @@ class DFColumnSelector(BaseEstimator, TransformerMixin):
 
 class PipelineLabelEncoder(LabelEncoder):
     """Wrapper around LabelEncoder to allow it to work within a Pipeline.
+
+    This class should be a drop-in replacement for LabelEncoder,
+    but should also work as part of a sklearn Pipeline.
     """
     def fit(self, y, *args, **kwargs):
         return super(PipelineLabelEncoder, self).fit(y)
@@ -42,3 +47,35 @@ class PipelineLabelEncoder(LabelEncoder):
 
     def fit_transform(self, y, *args, **kwargs):
         return super(PipelineLabelEncoder, self).fit_transform(y)
+
+
+class DataFrameConverter(BaseEstimator, TransformerMixin):
+    """A transformer to convert input data to a pandas.Dataframe
+    """
+    def fit(self, x, *args, **kwargs):
+        return self
+
+    def transform(self, x, *args, **kwargs):
+        return pd.DataFrame(x)
+
+
+class DFDummyEncoder(BaseEstimator, TransformerMixin):
+    """Dummy (aka one-hot) encoding of columns.
+    """
+    def __init__(self, columns=None, drop_first=None, prefix=None):
+        self.columns = columns
+        self.drop_first = drop_first
+        self.prefix = prefix
+
+    def fit(self, x, *args, **kwargs):
+        return self
+
+    def transform(self, x, *args, **kwargs):
+        if self.columns is None:
+            self.columns = x.columns
+        return pd.concat([x.drop(self.columns, axis=1),
+                          pd.get_dummies(x[self.columns],
+                                         prefix=self.prefix,
+                                         drop_first=self.drop_first,
+                                         columns=self.columns)],
+                         axis=1)
